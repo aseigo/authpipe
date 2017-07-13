bb4ab3bb6bfff61e0967a1bd6e77ea8620eb22d2
Aaron Seigo <aseigo@kde.org>
2017-07-13 11:44:08 +0200

    configuration!

---
+++ config/.credo.exs
@@ -0,0 +1,14 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: ["lib/"],
+        excluded: []
+      },
+      checks: [
+        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100},
+      ]
+    }
+  ]
+}
