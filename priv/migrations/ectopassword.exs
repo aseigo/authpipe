d2e7b4a445c97545075c1bcdcdc00fe8497c7ec5
Aaron Seigo <aseigo@kde.org>
2017-07-14 12:12:11 +0200

    improve the sql ddl

--- priv/migrations/ectopassword.exs
+++ priv/migrations/ectopassword.exs
@@ -3,9 +3,9 @@ defmodule <%= repo %>.Migrations.EctoPassword do
 
   def change do
     create table(:authpipe_stage_ectopassword, primary_key: false) do
-      add :user, :text
-      add :password, :text
-      add :active, :boolean
+      add :user, :text, primary_key: true, null: false
+      add :password, :text, null: false, default: ""
+      add :active, :boolean, default: false
     end
   end
 end
